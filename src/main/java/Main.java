5d37cd9659d4f4bb030329823e1bafb7f960de55
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2022-10-19 16:19:45 -0500

    maven exercise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         String test =  StringUtils.upperCase("lowertoupper");
         System.out.println(test);
+
+        System.out.println("enter an input to check if it's a number");
+        Scanner scandy = new Scanner(System.in);
+        String userInput = scandy.nextLine();
+        Boolean isNumeric = StringUtils.isNumeric(userInput);
+        System.out.println(isNumeric);
+        System.out.println("enter a string");
+        String userString = scandy.nextLine();
+        String string = StringUtils.upperCase(userString);
+        System.out.println(string);
+        String string1 = StringUtils.lowerCase(userString);
+        System.out.println(string1);
+        String string2 = StringUtils.reverse(userString);
+        System.out.println(string2);
     }
 }
