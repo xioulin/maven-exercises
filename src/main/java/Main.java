a96ee982520f2111135fabb616c954a074950215
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2022-10-19 15:57:17 -0500

    maven exercises init

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String test =  StringUtils.upperCase("lowertoupper");
+        System.out.println(test);
+    }
+}
