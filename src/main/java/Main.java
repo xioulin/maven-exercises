3790ddd44bec10802a7f36eaf226cdb6f7048966
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2022-10-21 21:44:14 -0500

    stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
         String test =  StringUtils.upperCase("lowertoupper");
         System.out.println(test);
-
         System.out.println("enter an input to check if it's a number");
         Scanner scandy = new Scanner(System.in);
         String userInput = scandy.nextLine();
@@ -15,11 +14,10 @@ public class Main {
         System.out.println(isNumeric);
         System.out.println("enter a string");
         String userString = scandy.nextLine();
-        String string = StringUtils.upperCase(userString);
-        System.out.println(string);
-        String string1 = StringUtils.lowerCase(userString);
-        System.out.println(string1);
-        String string2 = StringUtils.reverse(userString);
-        System.out.println(string2);
+        String swapstring = StringUtils.swapCase(userString);
+        System.out.println("input cases are swapped: "+swapstring);
+        String reversestring = StringUtils.reverse(userString);
+        System.out.println("input is reversed: "+reversestring);
+        System.out.println("hello");
     }
 }
